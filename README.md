136c0127e4c7a577dcd74fe3b2a31304249a9395
Corwin <corwin0934@gmail.com>
2020-11-05 05:32:52 -0800

    Updating code from a new laptop. Image switching featuring Javascript Code on games.html, and more added misc code on about me and contact me

---
+++ README.md
@@ -0,0 +1,2 @@
+# PSGaming
+This is a README file about my opinions on video games. I have seperate video games from multiple genres here, giving my opinions on what the pros and cons are of each game. I'll also be giving out hints and tips to said games for anyone that would like to know about something they didn't know or understand. :) Most of these games are up to date new released or still trending, but I will still provide a year-to-date release around the game. Open "index.html" for opening sequence for the website.
---
+++ script.js
@@ -0,0 +1,63 @@
+function myFunction() {
+    var x = document.getElementById("mytopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+//* Calculator on CoD link *//
+
+function multiplyBy()
+{
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+        document.getElementById("result").innerHTML = num1 * num2;
+}
+
+function divideBy() 
+{ 
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+document.getElementById("result").innerHTML = num1 / num2;
+}
+
+function addby()
+{
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+document.getElementById("result").innerHTML = num1 + num2;
+}
+
+// These functions are for changing logos on the games.html page //
+
+function owlogochg()
+{
+        document.getElementById("overwatchMO").src="images/owlogochange.jpg";
+}
+
+function owlogoret()
+{
+        document.getElementById("overwatchMO").src="images/OW1.jpg";
+}
+
+function codchg()
+{
+        document.getElementById("codMO").src="images/codlogochange.jpg";
+}
+
+function codret()
+{
+        document.getElementById("codMO").src="images/codlogo.png";
+}
+
+function wowchg()
+{
+        document.getElementById("wowMO").src="images/wowlogochange.jpg";
+}
+
+function wowret()
+{
+        document.getElementById("wowMO").src="images/wowicon.jpg";
+}
